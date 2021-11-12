c54a08ec32cbba3162e5eb9feb484eaf3775dc01
Sergey Baykov <baykov-sv@yandex.ru>
2021-11-12 14:07:19 +0300

    Исправлены замечания к ДЗ 1.3

--- test_add_contact.py
+++ test_add_contact.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 import unittest
-#from contact import Contact
 from models import *
 
 
--- test_add_group.py
+++ test_add_group.py
@@ -2,7 +2,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import unittest
-#from group import Group
 from models import *
 
 class TestAddGroup(unittest.TestCase):
