6a1fe702c2b5f3097c6796341402d86c6af2de73
baykov-sv <baykov-sv@yandex.ru>
2022-01-10 19:39:29 +0300

    test_del_group from DB

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,11 +1,17 @@
 from model.group import Group
+from timeit import timeit
 
 def test_group_list(app, db):
 
     def clean(group):
         return Group(id=group.id, name=group.name.strip())
 
-    ui_list = app.group.get_group_list()
-    db_list = map(clean, db.get_group_list())
+    print(timeit(lambda: app.group.get_group_list(), number=1))
 
-    assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+    print(timeit(lambda: map(clean, db.get_group_list()), number=1000))
+
+    #ui_list = app.group.get_group_list()
+    #db_list = map(clean, db.get_group_list())
+
+    #assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+    assert False
