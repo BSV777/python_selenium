baae0213afa9c6aa3e0f37df022f6cf2d2cd9908
baykov-sv <baykov-sv@yandex.ru>
2022-01-10 16:37:27 +0300

    DBFixture

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,15 +1,12 @@
-import pymysql
+import pymysql.cursors
 
-print(pymysql.__version__)
+connection = pymysql.connect(host="lab", database="addressbook", user="root", password="")
 
-connection = pymysql.connect(host="lab", database="addressbook", user="root", passwd="")
-# connection.autocommit(1)
-#
-# try:
-#     cursor = connection.cursor()
-#     # cursor.execute("select * from group_list")
-#     # for row in cursor.fetchall():
-#     #     print(row)
-# finally:
-#     connection.close()
-#
+try:
+    cursor = connection.cursor()
+    cursor.execute("select * from group_list")
+    for row in cursor.fetchall():
+        print(row)
+finally:
+    connection.close()
+
