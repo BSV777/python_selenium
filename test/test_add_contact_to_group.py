b1f63deab487f17bcabf1d360a1b28dbc4dd484b
baykov-sv <baykov-sv@yandex.ru>
2022-01-16 18:10:43 +0300

    ДЗ 22 fixed

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -4,16 +4,16 @@ import random
 
 
 def test_add_contact_to_group(app, db, orm):
-
-    if len(db.get_contact_list()) == 0:
-        app.contact.create(Contact(firstname="test"))
-    contacts = orm.get_contact_list()
-    contact = random.choice(contacts)
-
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
     groups = orm.get_group_list()
     group = random.choice(groups)
+
+    if len(db.get_contact_list()) == 0:
+        app.contact.create(Contact(firstname="test"))
+
+    contacts = orm.get_contacts_not_in_group(group)
+    contact = random.choice(contacts)
 
     app.contact.add_contact_to_group(contact.id, group.id)
 
