9e87fd35714d04ab65aefe6d60b2ede1f2ffe8b8
Sergey Baykov <baykov-sv@yandex.ru>
2022-01-20 12:34:16 +0300

    ДЗ 29

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
+import allure
 from model import Group
+import pytest
 
-#import pytest
 #from data.add_group import testdata
 #from data.groups import constant as testdata
 
@@ -12,11 +13,16 @@ from model import Group
 
 def test_add_group(app, db, json_groups, check_ui):
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
+    with allure.step('Given a group list'):
+        old_groups = db.get_group_list()
+    with allure.step('When I add a group %s to the list' % group):
+        app.group.create(group)
+
     #assert len(old_groups) + 1 == app.group.count()
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-    if check_ui:
-        assert sorted(app.group.get_group_list(), key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+    with allure.step('Then the new group list is equal to the old list with the added group'):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+        if check_ui:
+            assert sorted(app.group.get_group_list(), key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
